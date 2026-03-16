e0eb992792f808b3f4f90f80c36278885b7d0da0
DEXx-s <ryan123l.somsantos@gmail.com>
2026-03-16 18:08:09 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,42 +1,58 @@
 const express = require("express")
+const puppeteer = require("puppeteer")
 
 const app = express()
-const PORT = process.env.PORT || 3000
 
-app.use(express.static(__dirname))
+app.use(express.static("public"))
 
-app.get("/oferta", async (req,res)=>{
+app.get("/produto", async (req,res)=>{
 
 const url = req.query.url
 
 try{
 
-const api = "https://api.microlink.io?url=" + encodeURIComponent(url)
-
-const response = await fetch(api)
-
-const data = await response.json()
-
-const title = data.data.title || "Produto Shopee"
-const image = data.data.image?.url || ""
-
-res.json({
-title,
-image,
-price:"",
-url
+const browser = await puppeteer.launch({
+args:["--no-sandbox"]
 })
+
+const page = await browser.newPage()
+
+await page.goto(url,{waitUntil:"domcontentloaded"})
+
+await page.waitForTimeout(4000)
+
+const data = await page.evaluate(()=>{
+
+let titulo = document.querySelector("title")?.innerText || ""
+
+let preco = document.body.innerText.match(/R\$ ?[0-9]+[,\.][0-9]+/)
+
+let imagem = document.querySelector("img")?.src || ""
+
+return{
+titulo,
+preco:preco ? preco[0] : "",
+imagem
+}
+
+})
+
+await browser.close()
+
+res.json(data)
 
 }catch(e){
 
 res.json({
-title:"Erro ao pegar produto",
-image:"",
-price:""
+titulo:"Produto Shopee",
+preco:"",
+imagem:""
 })
 
 }
 
 })
 
-app.listen(PORT,()=>console.log("Servidor rodando"))
+app.listen(3000,()=>{
+console.log("Servidor rodando")
+})
