f76d8bb25b6d5da0f24c2f58a91854ec05a7bd59
DEXx-s <ryan123l.somsantos@gmail.com>
2026-03-17 18:01:14 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express = require("express")
+const axios = require("axios")
+const cheerio = require("cheerio")
+
 const app = express()
 
-app.use(express.static(__dirname))
+app.get("/produto", async (req, res)=>{
+  const url = req.query.url
 
-app.get("/", (req, res)=>{
-res.sendFile(__dirname + "/index.html")
+  try {
+    const { data } = await axios.get(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0"
+      }
+    })
+
+    const $ = cheerio.load(data)
+
+    const titulo = $('meta[property="og:title"]').attr("content")
+    const imagem = $('meta[property="og:image"]').attr("content")
+
+    let preco = $("body").text().match(/R\$ ?\d+[,\.]\d+/)?.[0]
+
+    res.json({
+      titulo,
+      preco,
+      imagem
+    })
+
+  } catch (e){
+    res.json({
+      titulo: "Produto Shopee",
+      preco: "",
+      imagem: ""
+    })
+  }
 })
 
 const PORT = process.env.PORT || 3000
-
-app.listen(PORT, ()=>{
-console.log("rodando...")
-})
+app.listen(PORT, ()=>console.log("API rodando"))
