f9203ee02504452169d5d4da2fbad0690eb980f3
DEXx-s <ryan123l.somsantos@gmail.com>
2026-03-17 17:58:57 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,62 +1,14 @@
 const express = require("express")
-const puppeteer = require("puppeteer")
-
 const app = express()
 
-app.use(express.static("."))
+app.use(express.static(__dirname))
 
-app.get("/produto", async (req,res)=>{
-
-try{
-
-const url = req.query.url
-
-const browser = await puppeteer.launch({
-args:["--no-sandbox","--disable-setuid-sandbox"]
-})
-
-const page = await browser.newPage()
-
-await page.goto(url,{waitUntil:"networkidle2"})
-
-// espera redirecionar
-await page.waitForTimeout(3000)
-
-const data = await page.evaluate(()=>{
-
-let titulo = document.querySelector('meta[property="og:title"]')?.content
-
-let imagem = document.querySelector('meta[property="og:image"]')?.content
-
-let preco = document.body.innerText.match(/R\$ ?\d+[,\.]\d+/)?.[0]
-
-return{
-titulo,
-imagem,
-preco
-}
-
-})
-
-await browser.close()
-
-res.json(data)
-
-}catch(e){
-
-res.json({
-titulo:"Produto Shopee",
-preco:"",
-imagem:""
-})
-
-}
-
+app.get("/", (req, res)=>{
+res.sendFile(__dirname + "/index.html")
 })
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT,()=>{
-console.log("Servidor rodando")
+app.listen(PORT, ()=>{
+console.log("rodando...")
 })
-// iniciarBot()
